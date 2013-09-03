513b2d193c1c7fa9489bbca613b2057c2283da23
Ludovic BOIS DE FER <ludovic.boisdefer@gmail.com>
2013-09-03 09:46:48 +0200

    test

--- filternet.js
+++ filternet.js
@@ -5,7 +5,8 @@ var myProxy = filternet.createProxyServer();
 // You can rewrite the request as it's being sent to the remote server.
 // (just headers)
 myProxy.on('interceptRequest', function (requestInfo, callback) {
- if(requestInfo.host.indexOf("boxee.tv")>-1){
+	console.log(requestInfo);
+ if(requestInfo.host.indexOf("boxee.tv")>-1 && requestInfo.path.indexOf("/api/get_recommendations")>-1){
    requestInfo.host = 'localhost';
    requestInfo.port = '9001';
  }
@@ -16,3 +17,5 @@ myProxy.on('error', function (error, locationInfo) {
  console.log(locationInfo);
  console.log(error.stack);
 });
+
+require('boxee-server');
